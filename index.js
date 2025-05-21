fe77a61667662187c1a8287ec50d32ec212bffc1
Aylon <aylonc1@gmail.com>
2025-05-22 01:08:14 +0300

    adding commits

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const { initRepo } = require('./lib/init');
 const { add } = require('./lib/add');
 const { help } = require('./lib/help');
+const { commit } = require('./lib/commit');
 
-const [,, command, target] = process.argv;
+const [,, command, ...args] = process.argv;
 
 switch (command) {
   case 'init':
     initRepo();
     break;
   case 'add':
-    add(target);
+    add(args[0]);
+    break;
+  case 'commit':
+    if(args[0] === '-m' && args[1]){
+       commit(args[1]);
+    }else{
+       console.log(`Usage: simplegit commit -m "message"`);
+    }
     break;
   case 'help':
+    help();
+    break;
   case undefined:
     help();
     break;
